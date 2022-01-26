677c36f4ff519952e2a42506824011b92ea47e2e
pavel <pgocev36@gmail.com>
2022-01-26 12:52:41 +0300

    Added userService

---
+++ src/api/UserService.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { getToken } from "../services/getToken";
+
+export enum userRoles {
+  worker = "work",
+  administrator = "admin"
+}
+
+interface IUserData {
+  fullName: string;
+  email: string;
+  password: string;
+  role: userRoles;
+}
+
+export default class UserService {
+  static #apiUrl = "https://mysterious-tundra-84714.herokuapp.com/user";
+  static async getAll() {
+    let config = {
+      headers: {
+        Authorization: getToken(),
+      },
+    };
+    const response = await axios.get(this.#apiUrl, config);
+    return response;
+  }
+  static async getUser(id: string) {
+    let config = {
+      headers: {
+        Authorization: getToken(),
+      },
+    };
+    const response = await axios.get(`${this.#apiUrl}/${id}`, config);
+    return response;
+  }
+  static async searchUser(text: string) {
+    let config = {
+      headers: {
+        Authorization: getToken(),
+      },
+    };
+    const response = await axios.get(`${this.#apiUrl}?search=${text}`, config);
+    return response;
+  }
+  static async addUser(data: IUserData) {
+    let config = {
+      headers: {
+        Authorization: getToken(),
+      },
+    };
+    const response = await axios.post(this.#apiUrl, data, config);
+    return response;
+  }
+  static async editUser(id: string, data: IUserData) {
+    let url = `${this.#apiUrl}/${id}`;
+    let config = {
+      headers: {
+        Authorization: getToken(),
+      },
+    };
+    const response = await axios.put(url, data, config);
+    return response;
+  }
+}
