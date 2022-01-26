e8f481c7f6db89dc31685fe0191a221c3f95daad
pavel <pgocev36@gmail.com>
2022-01-26 12:53:14 +0300

    CodeStyle chnge for AuthService

--- src/api/AuthService.ts
+++ src/api/AuthService.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-interface loginData {
+interface ILoginData {
     email: string,
     password: string
 }
 
-interface registrationData {
+interface IRegistrationData {
     fullName: string,
     email: string,
     password: string
@@ -13,13 +13,13 @@ interface registrationData {
 
 export default class AuthService {
     static #apiUrl = "https://mysterious-tundra-84714.herokuapp.com";
-    static async login(data:loginData) {
+    static async login(data:ILoginData) {
         const url = `${this.#apiUrl}/auth/login`
         const responce = await axios.post(url, data)
         console.log(responce.data);
         return responce
     }
-    static async register(data:registrationData) {
+    static async register(data:IRegistrationData) {
         const url = `${this.#apiUrl}/auth/registration`
         const responce = await axios.post(url, data)
         console.log(responce.data);
