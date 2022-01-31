f22b386c321184e02293b9f2c679411cbd234355
pavel <pgocev36@gmail.com>
2022-01-31 11:52:40 +0300

    ClaimService from js to ts

--- src/api/ClaimService.ts
+++ src/api/ClaimService.ts
@@ -1,6 +1,27 @@
 import axios from "axios";
 import { getToken } from "../services/getToken";
 
+export enum ClaimTypes {
+  hardware = "hard",
+  software = "soft",
+  networking = "net",
+  troubleshooting = "troublesh",
+}
+
+export enum ClaimStatuses {
+  declined = "decl",
+  new = "new",
+  done = "done",
+  inProgress = "in-progress",
+}
+
+interface IClaimData {
+  title: string;
+  type: ClaimTypes;
+  description: string;
+  status: ClaimStatuses;
+}
+
 export default class ClaimService {
   static #apiUrl = "https://mysterious-tundra-84714.herokuapp.com/claim";
   static async getAll() {
@@ -12,7 +33,7 @@ export default class ClaimService {
     const response = await axios.get(this.#apiUrl, config);
     return response;
   }
-  static async getClaim(id) {
+  static async getClaim(id: string) {
     let config = {
       headers: {
         Authorization: getToken(),
@@ -21,7 +42,7 @@ export default class ClaimService {
     const response = await axios.get(`${this.#apiUrl}/${id}`, config);
     return response;
   }
-  static async searchClaim(text) {
+  static async searchClaim(text: string) {
     let config = {
       headers: {
         Authorization: getToken(),
@@ -30,7 +51,7 @@ export default class ClaimService {
     const response = await axios.get(`${this.#apiUrl}?search=${text}`, config);
     return response;
   }
-  static async addClaim(data) {
+  static async addClaim(data: any) {
     let config = {
       headers: {
         Authorization: getToken(),
@@ -39,7 +60,7 @@ export default class ClaimService {
     const response = await axios.post(this.#apiUrl, data, config);
     return response;
   }
-  static async editClaim(id, data) {
+  static async editClaim(id: string, data: any) {
     let url = `${this.#apiUrl}/${id}`;
     let config = {
       headers: {
